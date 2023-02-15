be1951bf8e9398d60e3b6d609889f2386bb8440a
Musa Kuşbey <kusbeymusa7@gmail.com>
2023-02-15 13:51:40 +0300

    -P23-Xox_Game_With_React

---
+++ xoxgame/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+import Game from './components/Game' //Game klasörünü import edersen içindekiler otomatikmen import edilmiş olacak (index.js ve style.css)
+
+const App = () => {
+  return (
+    <Game />
+  )
+}
+
+export default App
